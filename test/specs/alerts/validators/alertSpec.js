7c260f23aa6bb1a75cf94ce98294883637c7e1f5
Bryan Ingle <bryan.ingle@barchart.com>
2018-08-02 09:54:57 -0500

    Remove invalid unit tests

--- test/specs/alerts/validators/alertSpec.js
+++ test/specs/alerts/validators/alertSpec.js
@@ -2,14 +2,4 @@ var alert = require('./../../../../lib/alerts/validators/alert');
 
 describe('When validating an alert object', () => {
 	'use strict';
-
-	describe('for the purpose of a user query', () => {
-		it('will throw an exception if the user "user_id" property is missing', function() {
-			expect(() => alert.forUser({ alert_system: 'barchart.com'})).toThrow();
-		});
-
-		it('will throw an exception if the user "alert_system" property is missing', function() {
-			expect(() => alert.forUser({ user_id: '123456'})).toThrow();
-		});
-	});
 });
