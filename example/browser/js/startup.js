a49c3f4b40aef78929c72c9543e74b11077f08b6
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-05-18 14:22:39 -0500

    Remove "debugger;" statement

--- example/browser/js/startup.js
+++ example/browser/js/startup.js
@@ -1103,8 +1103,6 @@ module.exports = (() => {
 							startupPromises.push(
 								alertManager.getUser()
 									.then(function(data) {
-										debugger;
-
 										if (data.user_id && data.alert_system) {
 											pageModel.authenticatedUser(`${data.user_id}@${data.alert_system}`);
 										}
