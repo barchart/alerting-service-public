688864ed7acccdc7f21683aa9ee39cdd2627d74a
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-08-03 09:58:12 -0500

    Remove self-executing wrapper function from example

--- example/node/example.js
+++ example/node/example.js
@@ -11,93 +11,89 @@ const JwtProvider = require('./../../lib/security/JwtProvider'),
 const LoggerFactory = require('./../../lib/logging/LoggerFactory'),
 	CustomLoggingProvider = require('./logging/CustomLoggingProvider');
 
-const startup = (() => {
-	'use strict';
-	
-	//LoggerFactory.configureForConsole();
-	//LoggerFactory.configureForSilence();
+//LoggerFactory.configureForConsole();
+//LoggerFactory.configureForSilence();
 
-	LoggerFactory.configure(new CustomLoggingProvider());
+LoggerFactory.configure(new CustomLoggingProvider());
 
-	const logger = LoggerFactory.getLogger('@barchart/example');
+const logger = LoggerFactory.getLogger('@barchart/example');
 
-	logger.info(`Example: Node.js example script started, SDK version [ ${AlertManager.version} ]`);
+logger.info(`Example: Node.js example script started, SDK version [ ${AlertManager.version} ]`);
 
-	let alertManager = null;
+let alertManager = null;
 
-	process.on('SIGINT', () => {
-		logger.info('Example: Processing SIGINT');
+process.on('SIGINT', () => {
+	logger.info('Example: Processing SIGINT');
 
-		if (alertManager !== null) {
-			alertManager.dispose();
-		}
-
-		logger.info('Example: Node.js example script ending');
-
-		process.exit();
-	});
-
-	process.on('unhandledRejection', (error) => {
-		logger.error('Unhandled Promise Rejection', error);
-	});
-
-	process.on('uncaughtException', (error) => {
-		logger.error('Unhandled Error', error);
-	});
-
-	const user_id = process.argv[2];
-	const alert_system = 'barchart.com';
-
-	const mode = process.argv[3];
-
-	let adapterClazz;
-	let adapterDescription;
-
-	if (mode === 'http') {
-		adapterClazz = AdapterForHttp;
-		adapterDescription = 'HTTP';
-	} else {
-		adapterClazz = AdapterForSocketIo;
-		adapterDescription = 'Socket.IO';
+	if (alertManager !== null) {
+		alertManager.dispose();
 	}
 
-	const host = 'alerts-management-demo.barchart.com';
-	const port = 443;
-	const secure = true;
+	logger.info('Example: Node.js example script ending');
 
-	logger.info(`Example: Created AlertManager for [ ${host}:${port} ] using [ ${adapterDescription} ] mode`);
+	process.exit();
+});
 
-	alertManager = new AlertManager(host, port, secure, adapterClazz);
+process.on('unhandledRejection', (error) => {
+	logger.error('Unhandled Promise Rejection', error);
+});
 
-	logger.info(`Example: Configuring JWT generator to impersonate [ ${user_id}@${alert_system} ]`);
+process.on('uncaughtException', (error) => {
+	logger.error('Unhandled Error', error);
+});
 
-	const jwtGenerator = getJwtGenerator(user_id, alert_system);
-	const jwtProvider = new JwtProvider(jwtGenerator, 60000, 'demo');
+const user_id = process.argv[2];
+const alert_system = 'barchart.com';
 
-	logger.info(`Example: Connecting to Barchart\'s Alert Service`);
+const mode = process.argv[3];
 
-	alertManager.connect(jwtProvider)
-		.then(() => {
-			logger.info(`Example: Connected to Barchart\'s Alert Service`);
+let adapterClazz;
+let adapterDescription;
 
-			logger.info(`Example: Retrieving a list of alerts for [ ${user_id}@${alert_system} ]`);
+if (mode === 'http') {
+	adapterClazz = AdapterForHttp;
+	adapterDescription = 'HTTP';
+} else {
+	adapterClazz = AdapterForSocketIo;
+	adapterDescription = 'Socket.IO';
+}
 
-			const payload = { };
-			
-			payload.user_id = user_id;
-			payload.alert_system = alert_system;
+const host = 'alerts-management-demo.barchart.com';
+const port = 443;
+const secure = true;
 
-			return alertManager.retrieveAlerts(payload)
-				.then((alerts) => {
-					logger.info(`Example: Retrieved alerts [ ${alerts.length} ] for [ ${user_id}@${alert_system} ]`);
-				}).catch((e) => {
-					logger.warn(`Example: Failed to retrieve alerts for [ ${user_id}@${alert_system} ]`);
-				});
-		}).catch((e) => {
-			logger.warn(`Example: Failed to connect to Barchart\'s Alert Service`);
-		}).then(() => {
-			logger.info(`Example: Disposing AlertManager`);
+logger.info(`Example: Created AlertManager for [ ${host}:${port} ] using [ ${adapterDescription} ] mode`);
 
-			alertManager.dispose();
-		});
-})();
+alertManager = new AlertManager(host, port, secure, adapterClazz);
+
+logger.info(`Example: Configuring JWT generator to impersonate [ ${user_id}@${alert_system} ]`);
+
+const jwtGenerator = getJwtGenerator(user_id, alert_system);
+const jwtProvider = new JwtProvider(jwtGenerator, 60000, 'demo');
+
+logger.info(`Example: Connecting to Barchart\'s Alert Service`);
+
+alertManager.connect(jwtProvider)
+	.then(() => {
+		logger.info(`Example: Connected to Barchart\'s Alert Service`);
+
+		logger.info(`Example: Retrieving a list of alerts for [ ${user_id}@${alert_system} ]`);
+
+		const payload = { };
+
+		payload.user_id = user_id;
+		payload.alert_system = alert_system;
+
+		return alertManager.retrieveAlerts(payload)
+			.then((alerts) => {
+				logger.info(`Example: Retrieved alerts [ ${alerts.length} ] for [ ${user_id}@${alert_system} ]`);
+			}).catch((e) => {
+				logger.warn(`Example: Failed to retrieve alerts for [ ${user_id}@${alert_system} ]`);
+			});
+	}).catch((e) => {
+		logger.warn(`Example: Failed to connect to Barchart\'s Alert Service`);
+	}).then(() => {
+		logger.info(`Example: Disposing AlertManager`);
+
+		alertManager.dispose();
+	});
