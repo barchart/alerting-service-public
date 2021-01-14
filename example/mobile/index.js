254ea8528f3fe7d6230b1042969630b73109a2f2
Dzmitry Afanasenka <dzmitry.afanasenka@itechart-group.com>
2021-01-14 19:46:51 +0300

    Ignore animation warning

--- example/mobile/index.js
+++ example/mobile/index.js
@@ -3,6 +3,9 @@ import { registerRootComponent } from 'expo';
 import App from './src/App';
 import { configureNotifications } from './src/utils/notifications';
 
+// eslint-disable-next-line no-console
+console.disableYellowBox = true;
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
