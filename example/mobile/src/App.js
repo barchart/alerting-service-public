b1843e8640961dc14ee22904e5489ca4499d08fe
Dzmitry Afanasenka <dzmitry.afanasenka@itechart-group.com>
2021-01-14 19:21:17 +0300

    Ignore animation warning

--- example/mobile/src/App.js
+++ example/mobile/src/App.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import { createStackNavigator } from '@react-navigation/stack';
+import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 
@@ -12,6 +13,8 @@ import { HomeScreen } from './screens/HomeScreen/HomeScreen';
 import { LatestPushNotification } from './screens/LatestPushScreen/LatestPushScreen';
 import { LoginScreen } from './screens/LoginScreen/LoginScreen';
 import { TriggersScreen } from './screens/TriggersScreen/TriggersScreen';
+
+LogBox.ignoreLogs(['Sending...']);
 
 import store from './redux';
 
