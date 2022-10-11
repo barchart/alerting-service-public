3705a3a9a1262a2f49a6bcaf2520844929542d5e
Bryan Ingle <bryaningle@gmail.com>
2022-10-11 15:59:55 -0600

    Release. Bump version number

--- test/SpecRunner.js
+++ test/SpecRunner.js
@@ -1,10 +1,8 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const assert = require('@barchart/common-js/lang/assert'),
-      is = require('@barchart/common-js/lang/is');
-
+  is = require('@barchart/common-js/lang/is');
 const condition = require('./condition'),
-      publisher = require('./publisher');
-
+  publisher = require('./publisher');
 module.exports = (() => {
   'use strict';
 
@@ -21,7 +19,6 @@ module.exports = (() => {
       assert.argumentIsOptional(alert.alert_behavior, `${d}.alert_behavior`, String);
       assert.argumentIsOptional(alert.automatic_reset, `${d}.automatic_reset`, Boolean);
       assert.argumentIsArray(alert.conditions, `${d}.conditions`, condition.forCreate);
-
       if (alert.hasOwnProperty('publishers')) {
         assert.argumentIsArray(alert.publishers, `${d}.publishers`, publisher.forCreate);
       }
@@ -43,7 +40,6 @@ module.exports = (() => {
       assert.argumentIsRequired(alert.alert_system, `${d}.alert_system`, String);
     }
   };
-
   function getDescription(description) {
     if (is.string(description)) {
       return description;
@@ -51,14 +47,12 @@ module.exports = (() => {
       return 'alert';
     }
   }
-
   return validator;
 })();
 
 },{"./condition":2,"./publisher":3,"@barchart/common-js/lang/assert":5,"@barchart/common-js/lang/is":6}],2:[function(require,module,exports){
 const assert = require('@barchart/common-js/lang/assert'),
-      is = require('@barchart/common-js/lang/is');
-
+  is = require('@barchart/common-js/lang/is');
 module.exports = (() => {
   'use strict';
 
@@ -73,19 +67,16 @@ module.exports = (() => {
       assert.argumentIsOptional(condition.property.target.kind, `${d}.property.target.kind`, String);
       assert.argumentIsRequired(condition.operator, `${d}.operator`, Object);
       assert.argumentIsRequired(condition.operator.operator_id, `${d}.operator.operator_id`, Number);
-
       if (Array.isArray(condition.operator.operand)) {
         assert.argumentIsArray(condition.operator.operand, `${d}.operator.operand`, String);
       } else {
         assert.argumentIsOptional(condition.operator.operand, `${d}.operator.operand`, String);
       }
-
       if (condition.operator.modifiers) {
         assert.argumentIsArray(condition.operator.modifiers, `${d}.operator.modifiers`, validateModifier);
       }
     }
   };
-
   function getDescription(description) {
     if (is.string(description)) {
       return description;
@@ -93,19 +84,16 @@ module.exports = (() => {
       return 'condition';
     }
   }
-
   function validateModifier(modifier, d) {
     assert.argumentIsRequired(modifier.modifier_id, `${d}.modifier_id`, Number);
     assert.argumentIsRequired(modifier.value, `${d}.value`, String);
   }
-
   return validator;
 })();
 
 },{"@barchart/common-js/lang/assert":5,"@barchart/common-js/lang/is":6}],3:[function(require,module,exports){
 const assert = require('@barchart/common-js/lang/assert'),
-      is = require('@barchart/common-js/lang/is');
-
+  is = require('@barchart/common-js/lang/is');
 module.exports = (() => {
   'use strict';
 
@@ -115,24 +103,19 @@ module.exports = (() => {
       assert.argumentIsRequired(publisher.type, `${d}.type`, Object);
       assert.argumentIsRequired(publisher.type.publisher_type_id, `${d}.type.publisher_type_id`, Number);
       assert.argumentIsRequired(publisher.use_default_recipient, `${d}.use_default_recipient`, Boolean);
-
       if (publisher.use_default_recipient === undefined || publisher.use_default_recipient === null) {
         publisher.use_default_recipient = false;
       }
-
       assert.argumentIsRequired(publisher.use_default_recipient, `${d}.use_default_recipient`, Boolean);
-
       if (publisher.use_default_recipient) {
         publisher.recipient = null;
       } else {
         assert.argumentIsRequired(publisher.recipient, `${d}.recipient`, String);
       }
-
       assert.argumentIsOptional(publisher.format, `${d}.format`, String);
       assert.argumentIsOptional(publisher.title, `${d}.title`, String);
     }
   };
-
   function getDescription(description) {
     if (is.string(description)) {
       return description;
@@ -140,15 +123,14 @@ module.exports = (() => {
       return 'publisher';
     }
   }
-
   return validator;
 })();
 
 },{"@barchart/common-js/lang/assert":5,"@barchart/common-js/lang/is":6}],4:[function(require,module,exports){
 const assert = require('@barchart/common-js/lang/assert');
-
 module.exports = (() => {
   'use strict';
+
   /**
    * An object which contains the required assertions for a token.
    *
@@ -157,7 +139,6 @@ module.exports = (() => {
    * @param {String} userId - The unique identifier of the authenticated user.
    * @param {String} alertSystem - The authenticated user's domain. In the demo environment, use your company name. In the production environment, Barchart will assign a value to use.
    */
-
   class JwtPayload {
     constructor(userId, alertSystem) {
       assert.argumentIsRequired(userId, 'userId', String);
@@ -165,6 +146,7 @@ module.exports = (() => {
       this._userId = userId;
       this._alertSystem = alertSystem;
     }
+
     /**
      * The unique identifier of the authenticated user. This value must match
      * the {@link Schema.Alert#user_id} of any alert you attempt to create, edit, or delete.
@@ -172,11 +154,10 @@ module.exports = (() => {
      * @public
      * @returns {String}
      */
-
-
     get userId() {
       return this._userId;
     }
+
     /**
      * The authenticated user's domain. In the demo environment, use your company name. This value must
      * match the {@link Schema.Alert#alert_system} of any alert you attempt to create, edit, or delete.
@@ -184,19 +165,16 @@ module.exports = (() => {
      * @public
      * @returns {String}
      */
-
-
     get alertSystem() {
       return this._alertSystem;
     }
+
     /**
      * Returns the simple object representation, used for signing a token.
      *
      * @public
      * @returns {String}
      */
-
-
     forSigning() {
       const serialized = {};
       serialized.user_id = this.userId;
@@ -205,19 +183,15 @@ module.exports = (() => {
       serialized.contextId = this.alertSystem;
       return serialized;
     }
-
     toString() {
       return '[JwtPayload]';
     }
-
   }
-
   return JwtPayload;
 })();
 
 },{"@barchart/common-js/lang/assert":5}],5:[function(require,module,exports){
 const is = require('./is');
-
 module.exports = (() => {
   'use strict';
 
@@ -250,30 +224,25 @@ module.exports = (() => {
       throwInvalidTypeError(variableName, typeDescription, index);
     }
   }
-
   function throwInvalidTypeError(variableName, typeDescription, index) {
     let message;
-
     if (typeof index === 'number') {
       message = `The argument [ ${variableName || 'unspecified'} ], at index [ ${index.toString()} ] must be a [ ${typeDescription || 'unknown'} ]`;
     } else {
       message = `The argument [ ${variableName || 'unspecified'} ] must be a [ ${typeDescription || 'Object'} ]`;
     }
-
     throw new Error(message);
   }
-
   function throwCustomValidationError(variableName, predicateDescription) {
     throw new Error(`The argument [ ${variableName || 'unspecified'} ] failed a validation check [ ${predicateDescription || 'No description available'} ]`);
   }
+
   /**
    * Utilities checking arguments.
    *
    * @public
    * @module lang/assert
    */
-
-
   return {
     /**
      * Throws an error if an argument doesn't conform to the desired specification (as
@@ -288,7 +257,6 @@ module.exports = (() => {
     argumentIsRequired(variable, variableName, type, typeDescription) {
       checkArgumentType(variable, variableName, type, typeDescription);
     },
-
     /**
      * A relaxed version of the "argumentIsRequired" function that will not throw if
      * the value is undefined or null.
@@ -303,32 +271,25 @@ module.exports = (() => {
       if (variable === null || variable === undefined) {
         return;
       }
-
       checkArgumentType(variable, variableName, type, typeDescription);
-
       if (is.fn(predicate) && !predicate(variable)) {
         throwCustomValidationError(variableName, predicateDescription);
       }
     },
-
     argumentIsArray(variable, variableName, itemConstraint, itemConstraintDescription) {
       this.argumentIsRequired(variable, variableName, Array);
-
       if (itemConstraint) {
         let itemValidator;
-
         if (typeof itemConstraint === 'function' && itemConstraint !== Function) {
           itemValidator = (value, index) => itemConstraint.prototype !== undefined && value instanceof itemConstraint || itemConstraint(value, `${variableName}[${index}]`);
         } else {
           itemValidator = (value, index) => checkArgumentType(value, variableName, itemConstraint, itemConstraintDescription, index);
         }
-
         variable.forEach((v, i) => {
           itemValidator(v, i);
         });
       }
     },
-
     /**
      * Throws an error if an argument doesn't conform to the desired specification
      * (as determined by a predicate).
@@ -344,32 +305,29 @@ module.exports = (() => {
         throwCustomValidationError(variableName, predicateDescription);
       }
     },
-
     areEqual(a, b, descriptionA, descriptionB) {
       if (a !== b) {
         throw new Error(`The objects must be equal [${descriptionA || a.toString()}] and [${descriptionB || b.toString()}]`);
       }
     },
-
     areNotEqual(a, b, descriptionA, descriptionB) {
       if (a === b) {
         throw new Error(`The objects cannot be equal [${descriptionA || a.toString()}] and [${descriptionB || b.toString()}]`);
       }
     }
-
   };
 })();
 
 },{"./is":6}],6:[function(require,module,exports){
 module.exports = (() => {
   'use strict';
+
   /**
    * Utilities for interrogating variables (e.g. checking data types).
    *
    * @public
    * @module lang/is
    */
-
   return {
     /**
      * Returns true if the argument is a number. NaN will return false.
@@ -382,7 +340,6 @@ module.exports = (() => {
     number(candidate) {
       return typeof candidate === 'number' && !isNaN(candidate);
     },
-
     /**
      * Returns true if the argument is NaN.
      *
@@ -394,7 +351,6 @@ module.exports = (() => {
     nan(candidate) {
       return typeof candidate === 'number' && isNaN(candidate);
     },
-
     /**
      * Returns true if the argument is a valid 32-bit integer.
      *
@@ -406,7 +362,6 @@ module.exports = (() => {
     integer(candidate) {
       return typeof candidate === 'number' && !isNaN(candidate) && (candidate | 0) === candidate;
     },
-
     /**
      * Returns true if the argument is a valid integer (which can exceed 32 bits); however,
      * the check can fail above the value of Number.MAX_SAFE_INTEGER.
@@ -419,7 +374,6 @@ module.exports = (() => {
     large(candidate) {
       return typeof candidate === 'number' && !isNaN(candidate) && isFinite(candidate) && Math.floor(candidate) === candidate;
     },
-
     /**
      * Returns true if the argument is a number that is positive.
      *
@@ -431,7 +385,6 @@ module.exports = (() => {
     positive(candidate) {
       return this.number(candidate) && candidate > 0;
     },
-
     /**
      * Returns true if the argument is a number that is negative.
      *
@@ -443,7 +396,6 @@ module.exports = (() => {
     negative(candidate) {
       return this.number(candidate) && candidate < 0;
     },
-
     /**
      * Returns true if the argument is iterable.
      *
@@ -455,7 +407,6 @@ module.exports = (() => {
     iterable(candidate) {
       return !this.null(candidate) && !this.undefined(candidate) && this.fn(candidate[Symbol.iterator]);
     },
-
     /**
      * Returns true if the argument is a string.
      *
@@ -467,7 +418,6 @@ module.exports = (() => {
     string(candidate) {
       return typeof candidate === 'string';
     },
-
     /**
      * Returns true if the argument is a JavaScript Date instance.
      *
@@ -479,7 +429,6 @@ module.exports = (() => {
     date(candidate) {
       return candidate instanceof Date;
     },
-
     /**
      * Returns true if the argument is a function.
      *
@@ -491,7 +440,6 @@ module.exports = (() => {
     fn(candidate) {
       return typeof candidate === 'function';
     },
-
     /**
      * Returns true if the argument is an array.
      *
@@ -503,7 +451,6 @@ module.exports = (() => {
     array(candidate) {
       return Array.isArray(candidate);
     },
-
     /**
      * Returns true if the argument is a Boolean value.
      *
@@ -515,7 +462,6 @@ module.exports = (() => {
     boolean(candidate) {
       return typeof candidate === 'boolean';
     },
-
     /**
      * Returns true if the argument is an object.
      *
@@ -527,7 +473,6 @@ module.exports = (() => {
     object(candidate) {
       return typeof candidate === 'object' && candidate !== null;
     },
-
     /**
      * Returns true if the argument is a null value.
      *
@@ -539,7 +484,6 @@ module.exports = (() => {
     null(candidate) {
       return candidate === null;
     },
-
     /**
      * Returns true if the argument is an undefined value.
      *
@@ -551,7 +495,6 @@ module.exports = (() => {
     undefined(candidate) {
       return candidate === undefined;
     },
-
     /**
      * Returns true if the argument is a zero-length string.
      *
@@ -563,7 +506,6 @@ module.exports = (() => {
     zeroLengthString(candidate) {
       return this.string(candidate) && candidate.length === 0;
     },
-
     /**
      * Given two classes, determines if the "child" class extends
      * the "parent" class (without instantiation).
@@ -575,13 +517,11 @@ module.exports = (() => {
     extension(parent, child) {
       return this.fn(parent) && this.fn(child) && child.prototype instanceof parent;
     }
-
   };
 })();
 
 },{}],7:[function(require,module,exports){
 const JwtPayload = require('./../../../lib/security/JwtPayload');
-
 describe('When constructing a JwtPayload', () => {
   'use strict';
 
@@ -622,7 +562,6 @@ describe('When constructing a JwtPayload', () => {
 
 },{"./../../../lib/security/JwtPayload":4}],8:[function(require,module,exports){
 const validator = require('./../../../lib/data/validators/alert');
-
 describe('When validating an alert object', () => {
   'use strict';
 
